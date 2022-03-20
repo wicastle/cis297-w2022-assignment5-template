6ef350ee316b003f15edc65d5cb6cf573d59d6df
symuriz <syed9648@gmail.com>
2022-03-20 15:13:50 -0400

    updated code for section 22.5

---
+++ BooksExamples/DisplayTable/DisplayAuthorsTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        public DisplayAuthorsTable()
+        {
+            InitializeComponent();
+        }
+
+        //Entity Framework DbContext
+        private BooksExamples.BooksEntities dbcontext = new BooksExamples.BooksEntities();
+        //load data from database into DataGridView
+        private void DisplayAuthorsTable_Load(object sender, EventArgs e)
+        {
+            //load Authors table ordered by LastName then FirstName
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+            //specify datasource for authorBindingSource
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+
+        }
+        private void authorBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+            //load Authors table ordered by LastName then FirstName
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+            //specify datasource for authorBindingSource
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+        }
+
+        
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            authorBindingSource.EndEdit();
+            try
+            {
+                dbcontext.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException)
+            {
+                MessageBox.Show("FirstName and LastName must contain values", "Entity Validation Exception");
+            }
+        }
+    }
+}
